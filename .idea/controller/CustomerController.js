e399df68006b32b96f77bdda02304ebec231bd24
Binu0714 <binuj714@gmail.com>
2025-05-09 20:57:37 +0530

    implement saveCustomer function

--- .idea/controller/CustomerController.js
+++ .idea/controller/CustomerController.js
@@ -20,3 +20,32 @@ function loadCustomers() {
         $('#customer-tbody').append(data);
     })
 }
+
+function nextId() {
+    let id;
+
+    if (customer_db.length>0){
+        const lastId = customer_db[customer_db.length-1].id;
+        id = parseInt(lastId.slice(1))+1;
+        id = 'C'+id.toString().padStart(3,'0');
+    }else {
+        id = 'C001';
+    }
+    return id;
+}
+
+$('#c_save').on('click',function (){
+    let id = nextId();
+    var name = $('#name').val();
+    var address = $('#address').val();
+    var nic = $('#nic').val();
+    var phone = $('#phone').val();
+
+    let customer_data = new CustomerModel(id,name,address,nic,phone);
+    customer_db.push(customer_data);
+
+    loadCustomers();
+
+    console.log(customer_data);
+    console.log(customer_db);
+})
